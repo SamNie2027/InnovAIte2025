71e5eee8092d2d89dae9ce06c05d5610d0ce1307
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-02-16 09:53:07 -0500

    changing host

--- TrashCanAPI/app.py
+++ TrashCanAPI/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-port = 4000
+port = 5000
 app = Flask(__name__)
 
 @app.route('/submitImage', methods=['POST'])
@@ -23,7 +23,7 @@ def submit_image(fullness_model, compare_model):
     return 'received data'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
     # initialize models
     # fullness_model = FullnessModel()
     # compare_model = CompareImagesModel()
