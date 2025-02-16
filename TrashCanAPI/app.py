44bd4cd38eccbcaad81e35db366f3eebfbed6992
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-02-16 13:18:07 -0500

    Added name

--- TrashCanAPI/app.py
+++ TrashCanAPI/app.py
@@ -39,3 +39,6 @@ def submit_image():
     except Exception as e:
         return jsonify({"error": str(e)}), 500  # Internal Server Error
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
