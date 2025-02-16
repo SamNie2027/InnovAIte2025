effa6a94a43007baede4ab4061eda14ac9108152
Sam Nie <147653722+SamNie2027@users.noreply.github.com>
2025-02-15 20:22:40 -0500

    Added index.js

--- frontend/src/components/ImageButton.tsx
+++ frontend/src/components/ImageButton.tsx
@@ -1,18 +1,14 @@
-<<<<<<< Updated upstream
-import { Button, Pressable, StyleSheet, Text } from "react-native";
-import { useCameraPermission } from "react-native-vision-camera";
-import { useFonts } from "expo-font";
-import useLocation from "../hooks/useLocation";
-=======
-import { Button, Pressable, StyleSheet, Text } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, useState } from 'react-native';
 import { useFonts } from 'expo-font';
+import { useCameraPermissions, CameraType } from 'expo-camera';
 import useLocation from '../hooks/useLocation';
->>>>>>> Stashed changes
 
 export const ImageButton = () => {
   const [fonts] = useFonts({
     Lexend: require("../../assets/fonts/Lexend/Lexend-VariableFont_wght.ttf"),
   });
+
+  const [facing, setFacing] = useState<CameraType>('back');
 
   const getAllPermissions = async () => {
     requestPermission();
@@ -22,16 +18,6 @@ export const ImageButton = () => {
         }*/
   };
 
-<<<<<<< Updated upstream
-  const { hasPermission, requestPermission } = useCameraPermission();
-
-  return (
-    <Pressable style={styles.button} onPress={getAllPermissions}>
-      <Text style={styles.buttonText}>Take a picture!</Text>
-    </Pressable>
-  );
-};
-=======
     const [permission, requestPermission] = useCameraPermissions();
     
     return (
@@ -40,32 +26,10 @@ export const ImageButton = () => {
         </Pressable>
     )
 }
->>>>>>> Stashed changes
 
 export default ImageButton;
 
 const styles = StyleSheet.create({
-<<<<<<< Updated upstream
-  button: {
-    marginTop: "10%",
-    width: "70%",
-    height: "15%",
-    borderColor: "#596869",
-    borderStyle: "solid",
-    backgroundColor: "white",
-    borderRadius: 20,
-    borderWidth: 8,
-    boxShadow: "0px 4px #000000",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonText: {
-    fontFamily: "Lexend",
-    fontWeight: 800,
-    fontSize: 25,
-  },
-});
-=======
     button: {
         marginTop: '10%',
         width: '70%',
@@ -89,4 +53,3 @@ const styles = StyleSheet.create({
 function useCameraPermissions(): [any, any] {
     throw new Error('Function not implemented.');
 }
->>>>>>> Stashed changes
