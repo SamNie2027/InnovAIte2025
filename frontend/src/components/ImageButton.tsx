d106298043c67518269f639cc7c12ebc40397624
Quantalope <nickywong1726@gmail.com>
2025-02-15 20:19:10 -0500

    camera
    camera

--- frontend/src/components/ImageButton.tsx
+++ frontend/src/components/ImageButton.tsx
@@ -1,7 +1,13 @@
+<<<<<<< Updated upstream
 import { Button, Pressable, StyleSheet, Text } from "react-native";
 import { useCameraPermission } from "react-native-vision-camera";
 import { useFonts } from "expo-font";
 import useLocation from "../hooks/useLocation";
+=======
+import { Button, Pressable, StyleSheet, Text } from 'react-native';
+import { useFonts } from 'expo-font';
+import useLocation from '../hooks/useLocation';
+>>>>>>> Stashed changes
 
 export const ImageButton = () => {
   const [fonts] = useFonts({
@@ -16,6 +22,7 @@ export const ImageButton = () => {
         }*/
   };
 
+<<<<<<< Updated upstream
   const { hasPermission, requestPermission } = useCameraPermission();
 
   return (
@@ -24,10 +31,21 @@ export const ImageButton = () => {
     </Pressable>
   );
 };
+=======
+    const [permission, requestPermission] = useCameraPermissions();
+    
+    return (
+        <Pressable style={styles.button} onPress={getAllPermissions}>
+            <Text style={styles.buttonText}>Take a picture!</Text>
+        </Pressable>
+    )
+}
+>>>>>>> Stashed changes
 
 export default ImageButton;
 
 const styles = StyleSheet.create({
+<<<<<<< Updated upstream
   button: {
     marginTop: "10%",
     width: "70%",
@@ -47,3 +65,28 @@ const styles = StyleSheet.create({
     fontSize: 25,
   },
 });
+=======
+    button: {
+        marginTop: '10%',
+        width: '70%',
+        height: '15%',
+        borderColor: '#596869',
+        borderStyle: 'solid',
+        backgroundColor: 'white',
+        borderRadius: 20,
+        borderWidth: 8,
+        boxShadow: '0px 4px #000000',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        fontFamily: 'Lexend',
+        fontWeight: 800,
+        fontSize: 25,
+    }
+});
+
+function useCameraPermissions(): [any, any] {
+    throw new Error('Function not implemented.');
+}
+>>>>>>> Stashed changes
